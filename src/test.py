24aa84d3121b276d7719cc69faa9f0e8bcced2e1
AAryann <aryanj@yamha-motor-india.com>
2024-05-29 13:03:55 +0530

    init

---
+++ src/test.py
@@ -0,0 +1,40 @@
+import re
+
+bucket_name = ''
+output_folder = ''
+model_file_name = ''
+prefix = ''
+model_key = ''
+
+
+def set_input_folder_from_name(image_name):
+    arry= split_string_and_append_to_array(image_name)
+    client_name=arry[0]
+    project_name =arry[1]
+    model_file_name =arry[2]
+    input_folder =arry[3]
+    output_folder=arry[4]
+    file_type =arry[5]
+    prefix = 'Model_files'
+    model_key = f'{prefix}/{model_file_name}'
+
+    print(arry,client_name,project_name,model_file_name,input_folder,output_folder,file_type,prefix,model_key)
+    
+def split_string_and_append_to_array(input_string):
+    # Define the regex pattern to split by periods
+    pattern = r'\.'
+    
+    # Use re.split to split the input string by the pattern
+    split_strings = re.split(pattern, input_string)
+    
+    # Initialize an empty array
+    result_array = []
+    
+    # Append each substring to the array
+    for substring in split_strings:
+        result_array.append(substring)
+    
+    return result_array
+
+test_image_name = "client_name.project_name.model_name.input_folder.output_folder.fileType"
+set_input_folder_from_name(test_image_name)
